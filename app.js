7b28ed44ab41372b1583cf908a68911215438f12
markjanzer <mark.janzer@gmail.com>
2015-07-27 19:12:26 -0700

    Big ReWrite
    Changed the objects inside the food Array.
    Got rid of any functions in error.js.
    Added some more higher order functions.

--- app.js
+++ app.js
@@ -9,19 +9,51 @@ app.set('view engine', 'ejs');
 var food = [];
 var eaten = 0;
 var spoiled = 0; 
+var congratulate = false;
+var foodDays = [];
 
-// For the initial calling of the page
-// I need help because I res.render "firstKey: firstKey" and the other functions did not 
-// carry over to index.ejs
 app.get('/', function (req, res) { 
-  res.render('index', {food: food, eaten: eaten, spoiled: spoiled, 
-    daysTillExpiration: daysTillExpiration, firstKey: firstKey, DTEFromItem: DTEFromItem
+  res.render('index', {eaten: eaten, spoiled: spoiled, congratulate: congratulate, 
+    foodDays: foodDays
   });
 });
 
 //---------- Below are functions I wrote to help the app process the POST requests
 
-// This gets a date object and returns the # days till expiration
+function forEach (obj, callback) {
+  if (obj.isArray) {
+    for (var i = 0; i < obj.length; i++) {
+      callback(obj[i]);
+    }
+  } else {
+    for (var key in obj) {
+      callback(obj[key]);
+    }
+  }
+}
+
+// This is inspired by the higher order "all" property that I saw in Javascript Koans.
+function all(object, test) {    
+  var bool = true;
+  forEach(object, function (objValue) {
+    if (!test(objValue)) {
+      bool = false;
+    }
+  });
+  return bool;
+}
+
+function any(object, test) {
+  var bool = false;
+  forEach(object, function (objValue) {
+    if (test(objValue)) {
+      bool = true;
+    }
+  });
+  return bool;
+}
+
+// This gets a date object and returns the number of days till expiration
 function daysTillExpiration (expirationDate) {  
   var now = new Date();
   var exp = new Date(expirationDate);
@@ -29,92 +61,72 @@ function daysTillExpiration (expirationDate) {
   return (Math.ceil(left/(1000 * 60 * 60 * 24)))
 }
 
-// I was having difficulty calling the first property in objects, so I made this
-function firstKey(obj) {  
-  for (var key in obj)      
-    return key;            
-}
-
-//The prior two allow for this function: "Date Till Expiration From Item".
-//The input is something like {Potato: July 26, 2015}
-function DTEFromItem (foodItem) {
-  return daysTillExpiration(foodItem[firstKey(foodItem)]);
-}
-
-
-// This is inspired by the higher order all property that I saw in javascript Koans. I first
-// defined it using a forEach, but this is more efficient. I use this to congratulate the user
-// if they have no expired food in their fridge.
-function all(object, callback) {    
-  if (object.isArray) {            
-    for (i = 0; i < object.length; i++) {  
-      if (!callback(object[i])) {    
-        return false; }              
-    }
-  }
-  else {
-    for (var key in object) {
-      if (!callback(object[key])) {
-        return false; }
-    }
-  }
-  return true;
-};
-
-// Finds the index of a food item. Needed for ate and spoiled buttons.
-function returnIndexOf (foodName) {
+// Finds and removes an item from food.
+function removeItem (foodName) {
+  var index = undefined;
   for (var i = 0; i < food.length; i++) {
-    if (firstKey(food[i]) === foodName) {
-      return i;
+    if (food[i].fName === foodName) {
+      food.splice(i, i + 1);
     }
   }
 }
 
-// Needed to identify if food
-function checkIfInFood (foodName) {
-  for (var i = 0; i< food.length; i++) {
-    if (firstKey(food[i]) === foodName)
-      return true;
-  }
-  return false;
-}
+/* This defines the boolean 'congratulate.' Congratulates the user if no food is spoiled,
+but there also has to be food in the fridge. 
+*/
+function doWeCongratulate () {
+  var noSpoiled = all(foodDays, function (foodItem) {
+    return (foodItem.eDate >= 0);
+  });
+  var notEmpty = (food.length > 0);
+  congratulate = (noSpoiled && notEmpty);
+} 
 
+/* Copying arrays and the objects within the arrays was a little difficult. I couldn't do 
+foodDays = food, so I push all of the objects into foodDays. However, objects also are tricky
+to copy. I found this way with JSON on the internet and it seems to work great. After I copy 
+the array (in a way that won't affect the original), I change the eDate value to be days until
+the expiration. index.ejs will use this array instead of the original 'food' array. */
+function defineFoodDays () {
+  foodDays = [];
+  forEach(food, function (foodItem) {
+    return foodDays.push((JSON.parse(JSON.stringify(foodItem))));
+  });
+  forEach(foodDays, function (foodItem) {
+    foodItem.eDate = daysTillExpiration(foodItem.eDate);
+  });
+}
 
 
 //----------------
 
 app.post('/', function (req, res) {
-  // If Post is fName, it adds the property and value to the food array
-  if (req.body.fName) {  
+  // This checks if the POST is from a submitted food item
+  if (req.body.fName && req.body.eDate) {  
     // This changes the name with a "*" until the name is unique
-    while (checkIfInFood(req.body.fName)) {
+    while (any(food, function (foodItem) {
+      return (foodItem.fName === req.body.fName);
+    })) {
       req.body.fName += "*"; 
-      console.log(req.body.fName);
     }
-    //Creat the individual object to addd it to the array.
-    var foodObject = {};
-    foodObject[req.body.fName] = req.body.expDate;
-    food.push(foodObject)
-  }
-    // If the req.body object has a key of Ate or Spoiled (if the buttons are pushed),
-    // then one of these carries out. These find the index of the item that is to be
-    // removed, uses it to splice and remove the object from the food array, and then
-    // adds 1 to the correlating counter. 
-  else if (req.body[firstKey(req.body)] === "Ate") { 
-    var indexOfFoodItem = returnIndexOf(firstKey(req.body));
-  	food.splice(indexOfFoodItem, indexOfFoodItem + 1);
-  	eaten += 1; }                       
-  else if (req.body[firstKey(req.body)] === "Spoiled") {
-  	var indexOfFoodItem = returnIndexOf(firstKey(req.body));
-    food.splice(indexOfFoodItem, indexOfFoodItem + 1);
+    food.push(req.body);
+  } else if (req.body.Status === "Ate") { 
+    removeItem(req.body.fName);
+  	eaten += 1; 
+  } else if (req.body.Status === "Spoiled") {
+  	removeItem(req.body.fName);
     spoiled += 1;
   }
-  // This sorts the food array using the .sort method. It sorts by expiration date in
-  // from earliest to latest. 
+  /* This sorts the food array using the .sort method. It sorts by expiration date in
+   from earliest to latest. */
   food.sort(function (a, b) {
-    return (daysTillExpiration(a[firstKey(a)]) - daysTillExpiration(b[firstKey(b)]));
+    return (daysTillExpiration(a.eDate) - daysTillExpiration(b.eDate));
   });
-  res.render('index', {food: food, eaten: eaten, spoiled: spoiled});
+  // Use these to define the variables foodDays and congratulate
+  defineFoodDays();
+  doWeCongratulate();
+  res.render('index', {food: food, foodDays: foodDays, eaten: eaten, spoiled: spoiled, 
+    congratulate: congratulate});
 });
 
 var server = app.listen(3000, function() {
